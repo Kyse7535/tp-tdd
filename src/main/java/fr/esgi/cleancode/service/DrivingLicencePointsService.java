2f73d770a60a644c1500b295349df23c6df8347c
trofinn <trofin.nicu98@gmail.com>
2022-11-30 21:40:22 +0100

    retirer points test red

---
+++ src/main/java/fr/esgi/cleancode/service/DrivingLicencePointsService.java
@@ -0,0 +1,2 @@
+package fr.esgi.cleancode.service;public class DrivingLicencePointsService {
+}
---
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicencePointsServiceTest.java
@@ -0,0 +1,44 @@
+package fr.esgi.cleancode.service;
+
+import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.ResourceNotFoundException;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
+
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class DrivingLicencePointsService {
+
+    @InjectMocks
+    private DrivingLicencePointsService service;
+    @InjectMocks
+    private DrivingLicenceFinderService finderService;
+
+    @Mock
+    private InMemoryDatabase database;
+
+    @Test
+    public void retirerPoints_for_not_found_drivingLicence() {
+
+        UUID id = UUID.randomUUID();
+        int nbr_points = 0;
+        when(database.findById(id)).thenReturn(Optional.empty());
+
+        Assertions.assertThrows(ResourceNotFoundException.class,() -> service.retirer(id,nbr_points));
+
+
+
+    }
+
+
+
+}
