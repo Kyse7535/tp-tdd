68ce12ad9ae8cdcda666ddf50d9e89dce01849a0
trofinn <trofin.nicu98@gmail.com>
2022-11-30 11:29:41 +0100

    test implementation for creation service and verifications

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreationServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceCreationServiceTest.java
@@ -1,6 +1,9 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.exception.InvalidDriverSocialSecurityNumberException;
+import fr.esgi.cleancode.model.DrivingLicence;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
@@ -16,34 +19,39 @@ public class DrivingLicenceCreationServiceTest {
     private InMemoryDatabase database;
 
     @Test
-    public void verification() {
-
+    public void should_have_valid_social_number() {
+        String social_securiy_number = "123456789101112";
+        Assertions.assertTrue(true,service.check_ss(social_securiy_number));
     }
 
     @Test
     public void create() {
+        String social_securiy_number = "123456789101112";
+        DrivingLicence drivingLicence = service.create(social_securiy_number);
+
+        Assertions.assertEquals(drivingLicence.getAvailablePoints(), 12);
+        Assertions.assertEquals(drivingLicence.getDriverSocialSecurityNumber(), "123456789101112");
 
     }
 
     @Test
     public void should_not_be_null() {
-
+        String social_securiy_number = "";
+        Assertions.assertThrows(InvalidDriverSocialSecurityNumberException.class,service.check_ss(social_securiy_number));
     }
 
     @Test
     public void should_contain_only_digits() {
-
+        String social_securiy_number = "aaaaaaaaaaaaaaa";
+        Assertions.assertThrows(InvalidDriverSocialSecurityNumberException.class,service.check_ss(social_securiy_number));
     }
 
     @Test
     public void should_have_15_digits() {
-
+        String social_securiy_number = "11111";
+        Assertions.assertThrows(InvalidDriverSocialSecurityNumberException.class,service.check_ss(social_securiy_number));
     }
 
-    @Test
-    public void should_raise_exception_if_ss_invalid() {
-
-    }
 
 
 }
