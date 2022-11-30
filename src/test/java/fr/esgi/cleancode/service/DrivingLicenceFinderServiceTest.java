00406646c807408964470355ec7c35902f4c4b9e
trofinn <trofin.nicu98@gmail.com>
2022-11-30 11:00:58 +0100

    find licence tests implementattion

--- src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
+++ src/test/java/fr/esgi/cleancode/service/DrivingLicenceFinderServiceTest.java
@@ -1,11 +1,16 @@
 package fr.esgi.cleancode.service;
 
 import fr.esgi.cleancode.database.InMemoryDatabase;
+import fr.esgi.cleancode.model.DrivingLicence;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+import java.util.UUID;
 
 @ExtendWith(MockitoExtension.class)
 class DrivingLicenceFinderServiceTest {
@@ -18,11 +23,28 @@ class DrivingLicenceFinderServiceTest {
 
     @Test
     void should_find() {
+        //BEFORE
+        DrivingLicence drivingLicence = service.create("12345");
+        UUID id = drivingLicence.getId();
+        database.save(drivingLicence.getId(),drivingLicence);
 
+        // EXECUTE
+        Optional<DrivingLicence> opt = service.findById(drivingLicence.getId());
+
+        // ASSERT
+        Assertions.assertEquals(opt.get(),drivingLicence);
     }
 
     @Test
     void should_not_find() {
+        // BEFORE
+        DrivingLicence drivingLicence = service.create("12345");
+        UUID id = drivingLicence.getId();
 
+        // EXECUTE
+        Optional<DrivingLicence> opt = service.findById(id);
+
+        // ASSERT
+        Assertions.assertNotEquals(opt.get(),drivingLicence);
     }
 }
